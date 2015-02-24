c3226ae8c80fd8bdd57282823dca31f5250c33b2
Igorisius <Sakivskyy.igor@gmail.com>
2015-02-24 18:47:42 +0200

    add separate folder for views

---
+++ students/views/groups.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Views for Groups
+def groups_list(request):
+    groups = (
+        {'id': 1,
+        'group_number': u'Іп 13',
+        'group_leader': u'Сергій Валігура'},
+        {'id': 2,
+        'group_number': u'Іф 42',
+        'group_leader': u'Стефа Зайчиха'},
+        {'id': 3,
+        'group_number': u'Іпф 23',
+        'group_leader': u'Володимир Перший'},
+    )   
+    return render(request, 'students/groups_list.html',
+        {'groups': groups})
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
--- students/views/students.py
+++ students/views/students.py
@@ -34,29 +34,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-
-# Views for Groups
-def groups_list(request):
-    groups = (
-        {'id': 1,
-        'group_number': u'Іп 13',
-        'group_leader': u'Сергій Валігура'},
-        {'id': 2,
-        'group_number': u'Іф 42',
-        'group_leader': u'Стефа Зайчиха'},
-        {'id': 3,
-        'group_number': u'Іпф 23',
-        'group_leader': u'Володимир Перший'},
-    )   
-    return render(request, 'students/groups_list.html',
-        {'groups': groups})
-
-def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
-# Create your views here.
