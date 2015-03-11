1dee11ac20d548ab7dcac878ef74cafc0bff42b1
Igorisius <Sakivskyy.igor@gmail.com>
2015-03-12 01:00:06 +0200

    add Group model

--- students/models.py
+++ students/models.py
@@ -49,4 +49,32 @@ class Student(models.Model):
     def __unicode__(self):
         return u"%s %s" % (self.first_name, self.last_name)
 
-# Create your models here.
+
+class Group(models.Model):
+    """Student Model"""
+
+    class Meta(object):
+        verbose_name = u"Група"
+        verbose_name_plural = u"Групи"
+
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Ім'я")
+
+    leader = models.OneToOneField('Student',
+        verbose_name=u"Староста",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL)
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки")
+    
+    def __unicode__(self):
+        if self.leader:
+            return u"%s (%s %s)" % (self.title, self.leader.first_name, self.leader.last_name)
+        else:
+            return u"%s" % (self.title,)
+
